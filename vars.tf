3239f6a06391668d86ded1251f352b3248e197db
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2020-11-22 17:08:34 +0000

    Add simple terraform code.

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  value = "dptest"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  value = "northeurope"
+}
