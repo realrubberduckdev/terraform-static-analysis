0447acd92a0a2c4a9f208a6e74d770f59b0bf94f
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2020-11-22 18:02:29 +0000

    Fix path.

--- RunTFSec.ps1
+++ RunTFSec.ps1
@@ -1,3 +1,3 @@
-$resultFileFullPath = Join-Path (Get-Location).Path $resultFile "tfsec-result.xml"
+$resultFileFullPath = Join-Path (Get-Location).Path "tfsec-result.xml"
 docker run --rm -v "$(pwd):/src" liamg/tfsec /src --format JUnit | Out-File $resultFileFullPath
 Write-Host "Result written to $resultFileFullPath"
