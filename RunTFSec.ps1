6a5e8ef06b17dc4e6ef940946d2cf6bf0c37a4a1
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2020-11-22 17:58:58 +0000

    Log full path to result file.

--- RunTFSec.ps1
+++ RunTFSec.ps1
@@ -1,1 +1,3 @@
-docker run --rm -v "$(pwd):/src" liamg/tfsec /src --format JUnit | Out-File tfsec-result.xml
+$resultFileFullPath = Join-Path (Get-Location).Path $resultFile "tfsec-result.xml"
+docker run --rm -v "$(pwd):/src" liamg/tfsec /src --format JUnit | Out-File $resultFileFullPath
+Write-Host "Result written to $resultFileFullPath"
